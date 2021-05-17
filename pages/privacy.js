e070c6a4b133f6dbfef1b8bd546e85a4212f1cfc
Joyson P S <joysonpathra@gmail.com>
2021-05-17 15:40:36 +0530

    adding privcy

---
+++ pages/privacy.js
@@ -0,0 +1,14 @@
+const privacy = () => {
+  return (
+    <div>
+      Privacy Policy Effective date: January 30, 2020 Joyson Demo ("us", "we",
+      or "our") operates the https://facebook-ashy.vercel.app/ websites and the
+      Facebbok clone mobile application (the "Service"). This page informs you
+      of our policies regarding the collection, use, and disclosure of personal
+      data when you use our Service and the choices you have associated with
+      that
+    </div>
+  );
+};
+
+export default privacy;
